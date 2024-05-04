bd6d9353a1ae06b7694f2cf35d10749b619705af
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-05-04 18:41:50 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,62 @@
 import React from 'react'
-import {FcLike} from "react-icons/fc";
+import {FcLike,FcLikePlaceholder} from "react-icons/fc";
+import { toast } from 'react-toastify';
 
  const Card = (props) => {
     let course = props.course;
+    let likedCourses = props.likedCourses;
+    let setLikedCourses = props.setLikedCourses;
+
+    function clickHandler(){
+        // logic
+        if(likedCourses.includes(course.id)){
+            // pehle se like hua pada tha
+            setLikedCourses((prev) => prev.filter((cid) => (cid !== course.id)));
+            toast.warning("Like Removed");
+        }
+
+        else{
+            // pehle se liked nahi hai course
+            // insert karna hai ye course liked course me
+            if(likedCourses.length === 0){
+                setLikedCourses([course.id]);
+            }
+
+            else{
+                // non-empty pehle se
+                setLikedCourses((prev) => [...prev, course.id]);
+            }
+            toast.success("Liked Successfully");
+
+        }
+    }
 
   return (
-    <div>
-        <div>
+    <div className="w-[300px] bg-gray-800 rounded-md overflow-hidden" >
+        <div className='relative'>
             <img src = {course.image.url} alt="Not Found"></img>
 
-            <div>
-                <button>
-                    <FcLike fontSize="1.75rem"/>
+            <div className="w-[40px] h-[40px] bg-white rounded-full
+            absolute right-2 bottom-[-12px] grid place-items-center " >
+                <button onClick={clickHandler}>
+                {
+                    likedCourses.includes(course.id)?
+                    (<FcLike fontSize="1.75rem"/>):
+                    (<FcLikePlaceholder fontSize="1.75rem"/>)
+                }
+                    
                 </button>
             </div>
         </div>
-        <div>
-            <p>{course.title}</p>
-            <p>{course.description}</p>
+        <div className='p-4'>
+            <p className='text-white text-lg font-semibold leading-6 ' >{course.title}</p>
+            <p className=' text-white mt-2' >
+                {
+                    course.description.length>100 ?
+                    (course.description.substr(0,100)) + "...":
+                    (course.description)
+                }
+            </p>
         </div>
 
     </div>
